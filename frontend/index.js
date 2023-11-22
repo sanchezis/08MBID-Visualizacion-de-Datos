fa441d4b377132ae2254652c5675959096a4ac90
Israel Llorens <sanchezis@hotmail.com>
2023-11-22 16:03:53 +0100

    viz

--- frontend/index.js
+++ frontend/index.js
@@ -150,80 +150,37 @@ Plotly.newPlot('viz2', datab, layoutb);
 //Plotly.newPlot('viz10', datab, layoutb);
 //Plotly.newPlot('viz11', dataa, layouta);
 
-var geoJsonUrl =
-  'https://gis-calema.opendata.arcgis.com/datasets/59d92c1bf84a438d83f78465dce02c61_0.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D';
 
-// Your GeoJSON data for California regions (simplified example)
-var californiaGeoJSON = {
-  type: 'FeatureCollection',
-  features: [
-    // Include your GeoJSON features for each region in California
-    // Example feature:
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Polygon',
-        coordinates: [
-          // Add coordinates for the polygon of the region
-        ],
-      },
-      properties: {
-        regionName: 'Region 1',
-        value: 45, // Add your data value for this region
-      },
-    },
-    // Add more features for other regions
-  ],
-};
 
-// Set up data for the choropleth map
-var data = [
-  {
-    type: 'choropleth',
-    geojson: californiaGeoJSON,
-    locations: californiaGeoJSON.features.map(
-      (feature) => feature.properties.regionName
-    ),
-    z: californiaGeoJSON.features.map((feature) => feature.properties.value),
-    colorscale: 'Viridis', // You can choose a different colorscale
-    colorbar: { title: 'Data Value' },
-  },
-];
 
-// Fetch GeoJSON data asynchronously using the fetch API
-fetch(geoJsonUrl)
-  .then((response) => response.json())
-  .then((geojson) => {
-    // Set up data for the choropleth map
-    //console.log(geojson); // name
-    var data = [
-      {
-        type: 'choropleth',
-        geojson: geojson,
-        locations: geojson.features.map(
-          (feature) => feature.properties.CountyName
-        ),
-        z: geojson.features.map((feature) => feature.geometry.coordinates),
-        colorscale: 'Viridis', // You can choose a different colorscale
-        colorbar: { title: 'Data Value' },
-      },
-    ];
+  // Fetch GeoJSON data
+  fetch("https://raw.githubusercontent.com/Esri/gis-tools-for-hadoop/master/samples/data/counties-data/california-counties.json")
+    .then(response => response.json())
+    .then(data => {
+      // Extract GeoJSON features
+      var geojsonFeatures = data.features;
+      // Extract feature properties (you may need to adjust this based on your GeoJSON structure)
+      var featureProperties = geojsonFeatures.map(feature => feature.attributes);
+      // Extract feature geometry
+      var featureGeometry = geojsonFeatures.map(feature => feature.geometry.rings);
+    
+    
+var map = L.map('map').setView([-119, 37.4], 10);
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-    // Set up layout for the map
-    var layout = {
-      geo: {
-        scope: 'California',
-        showland: true,
-        landcolor: 'rgb(217, 217, 217)',
-        subunitcolor: 'rgb(255,255,255)',
-        countrycolor: 'rgb(255,255,255)',
-        countrywidth: 0.5,
-        subunitwidth: 0.5,
-      },
-      margin: { l: 0, r: 0, b: 0, t: 0 },
-    };
+    console.log('OK');
 
-    // Create the choropleth map
-    Plotly.newPlot('choroplethMap', data, layout);
-  })
-  .catch((error) => console.error('Error loading GeoJSON:', error));
+    
+L.geoJson(featureGeometry).addTo(map);
+    
+    
+    
+    
+    
+    
+    
+    })
+    .catch(error => console.error('Error fetching GeoJSON:', error));
